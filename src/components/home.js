585ea2f0ab1e9a54b3f375e24539559a6245deaa
555ekstase <martinbibow@gmx.de>
2022-01-05 16:41:14 +0100

    vor Postionsversuch

--- src/components/home.js
+++ src/components/home.js
@@ -6,6 +6,8 @@ const home = () => {
         <div className='home'>
             <h1>Wähle dein Pokemon und bekämpfe alle anderen! Gibt eh zu viele.</h1>
             <Link to={`Hier ist dann ein link`} className="pupi" >FIGHT!</Link>
+            
+            <p className="highscorepos"><Link to={`Hier ist dann ein link zu den HS`} className="highscorelink" >Highscores</Link></p>
         </div>
     )
 }
