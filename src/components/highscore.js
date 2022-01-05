a867974d8323440e15111b59162c50173a9405e8
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-05 21:14:05 +0100

    Link im Highscore vergessen

--- src/components/highscore.js
+++ src/components/highscore.js
@@ -8,6 +8,8 @@ const highscore = (pokemon) => {
     return(
         <div>
             <h2>Hier kommt der Highscore rein</h2>
+            <Link to={"../auswahl"} className="zurück" >Kämpfe jetzt</Link>
+            <hr></hr>
             <Link to={"../"} className="zurück" >Zurück</Link>
         </div>
     )
