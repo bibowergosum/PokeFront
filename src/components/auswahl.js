047c771a6549afba73d55d82f48890716e0d1cac
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-06 14:56:33 +0100

    link in auswahl wieder eingefügt

--- src/components/auswahl.js
+++ src/components/auswahl.js
@@ -8,25 +8,18 @@ const auswahl = ({pokemon}) => {
     return(
         <div>
             <h2>Wähle dein Pokemon</h2>
+            <Link to={"arena"} className="zurück" >Fight now</Link>
+            <br></br>
+            <Link to={"../"} className="zurück" >Zurück</Link>
         {pokemon.map((poke, index) => (        
             <div className='post, container' key={index}>    
             <h2 className='title'>{poke.name.english}</h2>          
             <p>{poke.type[0]} {poke.type[1]}</p>
             {console.log(poke.type)} 
-            <Link to={`/auswahl/${poke.id}`} className="pupi" >DETAILS</Link>
-            
-            </div>
-            
-        ))}
-        
+            <Link to={`/auswahl/${poke.id}`} className="pupi" >DETAILS</Link>    
+            </div>       
+        ))}     
     </div>
-        // <div>
-        //     
-        //     
-        //     <Link to={"arena"} className="zurück" >Fight now</Link>
-        //     <hr></hr>
-        //     <Link to={"../"} className="zurück" >Zurück</Link>
-        // </div>
     )
 }
 
