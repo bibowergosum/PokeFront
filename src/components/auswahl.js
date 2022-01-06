1d4f704f0f094bcd7e5edd740bded31635b2cda1
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-06 14:48:39 +0100

    pokemon wählen fertig

--- src/components/auswahl.js
+++ src/components/auswahl.js
@@ -3,16 +3,30 @@ import {Link} from "react-router-dom"
 
 
 const auswahl = ({pokemon}) => {
+   
  
- console.log(pokemon)
     return(
         <div>
             <h2>Wähle dein Pokemon</h2>
-            <Link to={"../auswahl/:id"} className="zurück" >Detail</Link>
-            <Link to={"arena"} className="zurück" >Fight now</Link>
-            <hr></hr>
-            <Link to={"../"} className="zurück" >Zurück</Link>
-        </div>
+        {pokemon.map((poke, index) => (        
+            <div className='post, container' key={index}>    
+            <h2 className='title'>{poke.name.english}</h2>          
+            <p>{poke.type[0]} {poke.type[1]}</p>
+            {console.log(poke.type)} 
+            <Link to={`/auswahl/${poke.id}`} className="pupi" >DETAILS</Link>
+            
+            </div>
+            
+        ))}
+        
+    </div>
+        // <div>
+        //     
+        //     
+        //     <Link to={"arena"} className="zurück" >Fight now</Link>
+        //     <hr></hr>
+        //     <Link to={"../"} className="zurück" >Zurück</Link>
+        // </div>
     )
 }
 
--- src/components/auswahlID.js
+++ src/components/auswahlID.js
@@ -1,14 +1,18 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, useParams} from "react-router-dom"
 
 
-const auswahlID = (pokemon) => {
- 
- console.log(pokemon)
+const auswahlID = ({pokemon}) => {
+    const {id} = useParams
+    const poke = pokemon.filter((element) => {
+        console.log(pokemon.id)
+        return element.id === id
+    })
+    console.log()
     return(
         <div>
             <h2>Übersicht von deinem Pokemon</h2>
-
+            <h2>{poke[0]}</h2>
             <Link to={"detail"} className="zurück" >info</Link>
             <hr></hr>
             <Link to={"../auswahl"} className="zurück" >Zurück</Link>
