19f4ac4a72c9f2ccd2504b1b604fb8149ecc176e
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-10 12:04:52 +0100

    jetzt fehlt nur noch funktion

--- src/components/arena.js
+++ src/components/arena.js
@@ -6,6 +6,9 @@ import {Link, useParams} from "react-router-dom"
 const Arena = ({selectPokemon}) => {
     const id = {selectPokemon} 
     const [kampfPokemon, setKampfPokemon] = useState();
+    const [randomPokemon, setRandomPokemon] = useState();
+
+    var getRandomPokemon = Math.round(Math.random() * (809 - 1)) + 1;
 
     useEffect(() => {
        const fetchData = async() => {
@@ -15,15 +18,40 @@ const Arena = ({selectPokemon}) => {
        }
        fetchData();
       }, []);
-//  console.log(selectPokemon)
-//  console.log(kampfPokemon)
+
+
+      useEffect(() => {
+         const fetchData = async() => {
+          await axios.get (`https://pokedex1234.herokuapp.com/pokemon/${getRandomPokemon}`)
+          .then((res) => setRandomPokemon(res.data))
+          .catch((err) => console.log(err));
+         }
+         fetchData();
+        }, []);
+
 
     return(
         <>
         {kampfPokemon ? (<div className="App">
-        <h2></h2>      
+        <h2>{kampfPokemon.name.english}</h2>
+             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${kampfPokemon.id}.png`} className='pokeImage'></img>
+             <p>{kampfPokemon.type[0]} {kampfPokemon.type[1]} </p>
+             <p>HP: {kampfPokemon.base.HP}</p>
+             <p>Attack: {kampfPokemon.base.Attack}</p>
+             <p>Defense: {kampfPokemon.base.Defense}</p>  
               
-           
+             <h2>{randomPokemon.name.english}</h2>
+             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPokemon.id}.png`} className='pokeImage'></img>
+             <p>{randomPokemon.type[0]} {randomPokemon.type[1]} </p>
+             <p>HP: {randomPokemon.base.HP}</p>
+             <p>Attack: {randomPokemon.base.Attack}</p>
+             <p>Defense: {randomPokemon.base.Defense}</p>  
+
+
+
+
+             <Link to={"../"} className="zurück" >Nochmal Kämpfen</Link>
+
       </div>) : ("Loading...")}
       </>
     )
