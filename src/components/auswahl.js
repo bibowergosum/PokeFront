5cd8d965571a74c789e96d1a99ee46529653e024
555ekstase <martinbibow@gmx.de>
2022-01-11 14:23:24 +0100

    undo last commit

--- src/components/auswahl.js
+++ src/components/auswahl.js
@@ -21,8 +21,8 @@ const Auswahl = ({pokemon}) => {
         {pokemon.map((poke, index) => (        
             <div className='pokeselect' key={index}>    
             <h2 className='title'>{poke.name.english}</h2> 
-                     
-            <button value={poke.id} onClick={handleClick} type="button" ><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`}></img> </button>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`}></img>          
+            <button value={poke.id} onClick={handleClick} type="button">Auswählen</button>
             <Link to={`/auswahl/${poke.id}`} className="pupi2" >DETAILS</Link> 
                 
             </div>       
