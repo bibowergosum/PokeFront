85fbca6bbda3ec225778e93c99d50b3ee9b8bac1
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-12 17:17:40 +0100

    home überschrift geändert

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const home = () => {
     return (
         <div className='home'>
-            <h1>Wähle dein Pokemon und bekämpfe alle anderen! Gibt eh zu viele.</h1>
+            <h1>Choose your Pokemon and kill them all!</h1>
             <Link to={"Auswahl"} className="pupi" >FIGHT!</Link>
             
             <p className="highscorepos"><Link to={"highscore"} className="highscorelink" >Highscores</Link></p>
